34d6f9926b0dfb345af9ddb5b7eaf22af5823313
Alexander <asukhushin@icloud.com>
2022-10-22 13:06:06 +0300

    ДЗ №1 - Задача 4

---
+++ HomeWork 1.4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Домашнее задание 1
+
+// Задача 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите num1");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите num2");
+int num2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите num3");
+int num3 = int.Parse(Console.ReadLine()!);
+
+if(num1 > num2 &&  num2 > num3)
+{
+    Console.WriteLine($"{num1} => max из ряда чисел: {num1}, {num2}, {num3}");
+}
+
+if(num1 < num2 && num2 > num3)
+{
+    Console.WriteLine($"{num2} => max из ряда чисел: {num1}, {num2}, {num3}");
+}
+
+if(num1 < num2 && num2 < num3)
+{
+    Console.WriteLine($"{num3} => max из ряда чисел: {num1}, {num2}, {num3}");
+}
