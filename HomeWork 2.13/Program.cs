106af8f09c63edbe9fb8cd264b4ff07630a4aeeb
Alexander <asukhushin@icloud.com>
2022-10-23 17:37:37 +0300

    ДЗ №2 - Задача 13

--- HomeWork 2.13/Program.cs
+++ HomeWork 2.13/Program.cs
@@ -1,3 +1,24 @@
 ﻿// Задача 13. Напишите программу, которая выводит третью цифру заданного числа 
 // или сообщает, что третьей цифры нет.
 
+int GetThirdDigit(int number)
+{
+    if(number > 999)
+    {
+        while(number > 999)
+        {
+        number = number / 10;
+        }
+    }
+    if(number > 99 & number < 1000)
+    {
+        number = number % 10;
+    }
+    else
+    {
+        System.Console.Write("Нет третьей цифры в числе: ");
+    }
+    return number;
+}
+int myNumber = 248984;
+System.Console.WriteLine(GetThirdDigit(myNumber));
