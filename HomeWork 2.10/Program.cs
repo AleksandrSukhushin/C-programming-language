b7fe9e9c10ef0afed967f5c6057bb927d0ac38cd
Alexander <asukhushin@icloud.com>
2022-10-23 16:29:07 +0300

    ДЗ №2 - Задача 10

---
+++ HomeWork 2.10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10. Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+int GetLastDigit(int number)
+{
+    int LastDigit = number / 10;
+    return LastDigit;
+}
+int GetTwoDigit(int number)
+{
+    int TwoDigit = number % 10;
+    return TwoDigit;
+}
+int testNumber = 456;
+Console.WriteLine(GetTwoDigit(testNumber));
