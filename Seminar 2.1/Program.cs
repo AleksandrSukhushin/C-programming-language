13e1a71799500935ed73e71426de93ce0270507a
Alexander <asukhushin@icloud.com>
2022-10-23 15:03:05 +0300

    Определение последнего значения трехзначного числа

--- Seminar 2.1/Program.cs
+++ Seminar 2.1/Program.cs
@@ -3,12 +3,14 @@
 // Задача 1. Напишите программу, которая принимает на вход трехзначное число 
 //и на выходе показывает последнюю цифру этого числа.
 
-int GetLastDigit(int number);
+int GetLastDigit(int number)
 {
-    int lastDigit = number % 10;
-    return lastDigit;
+    //int lastDigit = number % 10;
+    // Можно просто: return number % 10 без верхней строчки;
+    //return lastDigit;
+    return number % 10;
 }
-int testNumber = 5123;
-int lastDigit = GetLastDigit(testNumber);
+int testNumber = 5123235;
+//int lastDigit = GetLastDigit(testNumber);
 
-System.Console.WriteLine();
+Console.WriteLine(GetLastDigit(testNumber));
