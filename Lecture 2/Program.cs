69640d40908cffaa829f50e0a47e6b743081529b
Alexander <asukhushin@icloud.com>
2022-10-23 12:33:11 +0300

    Функции и массивы

--- Lecture 2/Program.cs
+++ Lecture 2/Program.cs
@@ -1,2 +1,18 @@
 ﻿// Лекция №2. Функции и массивы
 
+int[] array = {1,12,31,4,18,15,16,17,18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
