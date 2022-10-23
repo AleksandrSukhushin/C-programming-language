537a5cba25d227cc1309f9473d207a81db100e42
Alexander <asukhushin@icloud.com>
2022-10-23 19:54:21 +0300

    ДЗ №2 - Задача 9 (дополнительная)

--- HomeWork 2.9 add/Program.cs
+++ HomeWork 2.9 add/Program.cs
@@ -1,3 +1,17 @@
 ﻿// Дополнительная Задача 9
 // Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
 
+int MulDigit(int number)
+{
+    if (number % 7 == 0 && number % 23 == 0)
+    {
+        System.Console.Write("Кратно одновременно 7 и 23, число: ");
+    }
+    else
+    {
+        System.Console.Write("Не кратно одновременно 7 и 23, число: ");
+    }
+    return number;
+}
+int myNumber = 165;
+System.Console.WriteLine(MulDigit(myNumber));
