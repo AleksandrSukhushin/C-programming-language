0aff2d856ef7fbe4e780766239b969a331921cfc
Alexander <asukhushin@icloud.com>
2022-10-23 15:38:46 +0300

    Вывод рандомного числа из отрезка + определение его большей цифры - 1 вариант рещения

--- Seminar 2.2/Program.cs
+++ Seminar 2.2/Program.cs
@@ -1,5 +1,23 @@
 ﻿// Семинар 2. Решение задач по теме Массивы и Функции
 
-// Задача 2. Напишите программу, которая выводит случайное число из отрезка [10,99]
+// Задача 2 (1). Напишите программу, которая выводит случайное число из отрезка [10,99]
 // и показывает наибольшую цифру числа.
 
+int BiggerDigit(int digit)
+{
+    int result = -1;
+    int num1 = digit % 10;
+    int num2 = digit / 10;
+    if(num1 > num2)
+    {
+        result = num1;
+    } 
+    else if(num2 > num1)
+    {
+        result = num2;
+    }
+    return result;
+}
+int randomDigit = new Random().Next(10,100);
+System.Console.WriteLine(randomDigit);
+System.Console.WriteLine(BiggerDigit(randomDigit));
