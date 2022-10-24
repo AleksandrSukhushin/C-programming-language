fd4240bc846f4319bdd57fe9f52044d351faf77f
Alexander <asukhushin@icloud.com>
2022-10-24 21:42:46 +0300

    Лекция 3

--- Lecture 3.1/Program.cs
+++ Lecture 3.1/Program.cs
@@ -14,4 +14,40 @@ void Method2(string msg)
 {
     System.Console.WriteLine(msg);
 }
-Method2("Текст сообщения");
+//Method2(msg: "Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i=0;
+    while (i < count)
+    {
+        System.Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method21(msg: "Текст сообщения", count: 4);
+
+// Метод 3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+//int year = Method3();
+//System.Console.WriteLine(year);
+
+// Методы 4
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+//string res = Method4(10, "0");
+//System.Console.WriteLine(res);
+
