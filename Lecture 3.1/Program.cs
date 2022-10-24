5bdfb240b01add69db4ef7e17f06a7709361004c
Alexander <asukhushin@icloud.com>
2022-10-24 21:27:47 +0300

    Лекция 3

--- Lecture 3.1/Program.cs
+++ Lecture 3.1/Program.cs
@@ -1,2 +1,17 @@
-﻿// Лекция 3. Функции: продолжение
+﻿// Лекция 3. Функции (методы): продолжение
 
+// Виды методов:
+
+// Метод 1
+void Method1 ()
+{
+    System.Console.WriteLine("Автор ...");
+}
+//Method1();
+
+// Метод 2
+void Method2(string msg)
+{
+    System.Console.WriteLine(msg);
+}
+Method2("Текст сообщения");
