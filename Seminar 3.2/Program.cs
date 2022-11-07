6da65a4187d35af8b98b4bcd5289aec0a9942ad9
Alexander <asukhushin@icloud.com>
2022-11-07 20:21:57 +0300

    Задача 18

---
+++ Seminar 3.2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон
+//возможных координат точек в этой четверти (x и y).
+
+void CoordinateRange(int numberRange)
+{
+    if(numberRange == 1) System.Console.WriteLine("Диапазон координат (+Х; +Y)");
+    else if(numberRange == 2) System.Console.WriteLine("Диапазон координат (-X; +Y)");
+    else if(numberRange == 3) System.Console.WriteLine("Диапазон координат (-X; -Y)");
+    else if(numberRange == 4) System.Console.WriteLine("Диапазон координат (+X; -Y)");
+}
+System.Console.WriteLine("Номер плоскости ");
+int numberRange = int.Parse(System.Console.ReadLine()!);
+CoordinateRange(numberRange);
