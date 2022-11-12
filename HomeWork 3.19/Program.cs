0ff98d7c6b3534ce305dd382e1fe2a4ec01f1e90
Alexander <asukhushin@icloud.com>
2022-11-12 12:56:01 +0300

    ДЗ №3 - Задача 19

--- HomeWork 3.19/Program.cs
+++ HomeWork 3.19/Program.cs
@@ -3,3 +3,28 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+void IsPalindromName(int number)
+{
+    if(number >= 10000)
+    {
+        int num1 = number / 10000;
+        int res1 = number % 10;
+            if(num1 == res1)
+            {
+                number = number / 10;
+                int num2 = (number / 100) % 10;
+                int res2 = number % 10;
+                if(num2 == res2)
+                    Console.WriteLine("It's a palindrome");
+            }
+            else
+            Console.WriteLine("It's not a palindrome");
+    }
+    else
+    Console.WriteLine("Необходимо пятизначное число!");
+}
+
+Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.ReadLine()!);
+IsPalindromName(number);
