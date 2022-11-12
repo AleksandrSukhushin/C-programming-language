0297d9adcf5752270a55c8a8efb265dc016600ea
Alexander <asukhushin@icloud.com>
2022-11-12 13:42:02 +0300

    ДЗ №3 - Дополнительная задача

--- HomeWork 3.ADD/Program.cs
+++ HomeWork 3.ADD/Program.cs
@@ -1,3 +1,25 @@
 ﻿// // Дополнительно.
 // Напишите программу, которая принимает на вход число (N) и выдает таблицу простых чисел от 1 до N.
 // 11 -> 2, 3, 5 , 7, 11
+
+bool PrimeNumbers(int number)
+{
+    int sqrtNumber = (int) (Math.Sqrt(number));
+    for (int i = 2; i <= sqrtNumber; i++)
+    {
+        if (number % i == 0)
+            return false;
+    }
+    return true;
+}
+int count = 0;
+int maxNumber = 11;
+for (int num = 2; num <= maxNumber; num++)
+{
+    if (PrimeNumbers(num))
+    {
+        count++;
+        Console.Write(num.ToString()+" ");
+    }
+}
+Console.WriteLine();
