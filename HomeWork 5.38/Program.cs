0c36a5de934057bca3ab124be6756edfc31c3b48
Alexander <asukhushin@icloud.com>
2022-11-20 14:56:17 +0300

    ДЗ №5 - Задача 38

--- HomeWork 5.38/Program.cs
+++ HomeWork 5.38/Program.cs
@@ -1,2 +1,37 @@
-﻿// Задача 38: Задайте массив вещественных чисел.
+﻿// Задача 38. Задайте массив вещественных чисел.
 // Найдите разницу между максимальным и минимальным элементов массива.
+
+Console.Clear();
+
+Console.Write("Введите количество элементов массива: ");
+int elements = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[elements];
+FillArrayRandomRealNumbers(numbers);
+Console.Write("Исходный массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max){max = numbers[i];}
+    if (numbers[i] < min){min = numbers[i];}
+}
+
+Console.WriteLine($"Разница между максимальным и минимальным элементами = {max - min}");
+
+void FillArrayRandomRealNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(10,100)) / 10;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + "  ");
+        }
+    Console.WriteLine();
+}
