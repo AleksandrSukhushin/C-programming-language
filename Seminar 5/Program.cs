7beafc71d1dc027e0d181ba123d93ed513c79991
Alexander <asukhushin@icloud.com>
2022-11-20 12:36:12 +0300

    Семинар 5: Задачи: 31, 35, 37

--- Seminar 5/Program.cs
+++ Seminar 5/Program.cs
@@ -27,37 +27,49 @@
 // Задача 31. Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9;9].
 // Найдите сумму отрицательных и положительных элементов массива.
 
-int length = 5;
-int[] massive = new int[length];
-FillArray(massive);
-PrintArray(massive);
-int sumPositive = SumOfSingNumbers(massive, true);
-int sumNegative = SumOfSingNumbers(massive, false);
-Console.WriteLine($"{sumPositive},{sumNegative}");
-int SumOfSingNumbers(int[] array, bool isPositive)
-{
-    int sum = 0;
-    foreach (var item in array)
-    {if(isPositive && item > 0) {sum += item;}
-    else if(!isPositive && item < 0){sum += item;}}
-    return sum;
-}
-void FillArray(int[] array, int minValue = -9, int maxValue = 9)
-{
-    var rnd = new Random();
-    for (int i = 0; i < array.Length; i++)
-    {array[i] = rnd.Next(minValue, maxValue + 1);}
-}
-int[] GetArray(int length, int minValue = -9, int maxValue = 9)
-{
-    int[] array = new int[length];
-    FillArray(array, minValue, maxValue);
-    return array;
-}
-void PrintArray(int[] array)
-{foreach (int element in array){Console.Write(element + " ");}Console.WriteLine();}
+// int length = 5;
+// int[] massive = new int[length];
+// FillArray(massive);
+// PrintArray(massive);
+// int sumPositive = SumOfSingNumbers(massive, true);
+// int sumNegative = SumOfSingNumbers(massive, false);
+// Console.WriteLine($"{sumPositive},{sumNegative}");
+// int SumOfSingNumbers(int[] array, bool isPositive)
+// {
+//     int sum = 0;
+//     foreach (var item in array)
+//     {if(isPositive && item > 0) {sum += item;}
+//     else if(!isPositive && item < 0){sum += item;}}
+//     return sum;
+// }
+// void FillArray(int[] array, int minValue = -9, int maxValue = 9)
+// {
+//     var rnd = new Random();
+//     for (int i = 0; i < array.Length; i++)
+//     {array[i] = rnd.Next(minValue, maxValue + 1);}
+// }
+// int[] GetArray(int length, int minValue = -9, int maxValue = 9)
+// {
+//     int[] array = new int[length];
+//     FillArray(array, minValue, maxValue);
+//     return array;
+// }
+// void PrintArray(int[] array)
+// {foreach (int element in array){Console.Write(element + " ");}Console.WriteLine();}
 
     // for (int i = 0; i < array.Length; i++)
     // {
     //     Console.WriteLine(array[i] + " ");
     // }
+
+
+// Задача 35. Задайте одномерный массив из 123 случайных чисел. Найдите количество двухзначных элементов массива.
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123.
+
+
+
+
+
+
+// Задача 37. Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент,
+// второй и предпоследний и т.д. Результат запишите в новом массиве.
