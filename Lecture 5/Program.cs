c5c87a37ea54f498a4f779b0790527563c019454
Alexander <asukhushin@icloud.com>
2022-11-20 09:45:14 +0300

    Как не нужно писать код: основные рекомендации

--- Lecture 5/Program.cs
+++ Lecture 5/Program.cs
@@ -1,1 +1,12 @@
 ﻿// Лекция 5.Как не нужно писать код: основные рекомендации
+
+// Тип переменной - определение
+
+using System.Linq;
+var a = 12;
+Console.WriteLine(a.GetType().Name);
+
+var data = new int[]{1,2,3,4}
+            .Where(e=> e>0)
+            .Select(e=> new{ q = e, w = e + 1});
+System.Console.WriteLine(data.GetType().Name);
