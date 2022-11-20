a2178ac2ecba6d292ead83c78950a17d124b13e0
Alexander <asukhushin@icloud.com>
2022-11-20 14:36:38 +0300

    ДЗ №5 - Задача 36

--- HomeWork 5.36/Program.cs
+++ HomeWork 5.36/Program.cs
@@ -1,2 +1,34 @@
 ﻿// Задача 36. Задайте одномерный массив, заполненный случайными числами.
 // Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.Clear();
+
+Console.Write("Введите количество элементов массива: ");
+int elements = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[elements];
+FillArrayRndNumbers(numbers);
+Console.Write("Исходный массив: ");
+PrintArray(numbers);
+
+int sum = 0;
+for (int i = 0; i < numbers.Length; i += 2)
+{
+    sum = sum + numbers[i];
+}
+Console.WriteLine($"Ответ: {sum}");
+
+void FillArrayRndNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(0,100);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
