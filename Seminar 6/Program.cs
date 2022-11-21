c6dab879b1735e104677be9b64a75976fd0053e5
Alexander <asukhushin@icloud.com>
2022-11-21 15:39:06 +0300

    Семинар 6: Задачи 39, 40, 41

--- Seminar 6/Program.cs
+++ Seminar 6/Program.cs
@@ -4,13 +4,10 @@
 // (последний элемент будет на первом месте, а первый - на последнем и тд).
 
 // Console.Clear();
-
 // int[] array = { 1, 2, 3, 4, 5 };
-
 // int[] copyOfArray = CopyRevertArray(array);
 // PrintArray(copyOfArray);
 // PrintArray(array);
-
 // int[] CopyRevertArray(int[] array)
 // {
 //     int[] copy = new int[array.Length];
@@ -29,22 +26,29 @@
 //         array[array.Length - 1 - i] = temp;
 //     }
 // }
-
 // void PrintArray(int[] array)
 // {
 //     System.Console.WriteLine(string.Join(", ", array));
 // }
 
+
 // Задача 40. Напишите программу, которая принимает на вход три числа и проверяет, может ли существоваться треугольник
 // со сторонами такой длины.
 // *Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
 
+Console.Clear();
 
+int[] arrayTriangle = new int[3] { 10, 4, 5 };
 
-
-
-
-
-
+bool isTriangle(int[] array)
+{
+    bool flag = true;
+    for (int i = 0; flag && i < array.Length; i++)
+    {
+        flag = arrayTriangle[i] < (array.Sum() - array[i]);
+    }
+    return flag;
+}
+Console.WriteLine(isTriangle(arrayTriangle));
 
 // Задача 42. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
