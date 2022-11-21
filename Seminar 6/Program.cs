285cefdf725880ac0b926fccbbb57625b9233b34
Alexander <asukhushin@icloud.com>
2022-11-21 14:52:46 +0300

    Семинар 6

--- Seminar 6/Program.cs
+++ Seminar 6/Program.cs
@@ -1,1 +1,46 @@
 ﻿// Семинар 6. Двумерные массивы и рекурсия
+
+// Задача 39. Напишите программу, которая перевернет одномерный массив
+// (последний элемент будет на первом месте, а первый - на последнем и тд).
+
+// Console.Clear();
+
+// int[] array = { 1, 2, 3, 4, 5 };
+
+// int[] copyOfArray = CopyRevertArray(array);
+// PrintArray(copyOfArray);
+// PrintArray(array);
+
+// int[] CopyRevertArray(int[] array)
+// {
+//     int[] copy = new int[array.Length];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         copy[i] = array[array.Length - 1 - i];
+//     }
+//     return copy;
+// }
+// void RevertArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length / 2; i++)
+//     {
+//         int temp = array[i];
+//         array[i] = array[array.Length - 1 - i];
+//         array[array.Length - 1 - i] = temp;
+//     }
+// }
+
+// void PrintArray(int[] array)
+// {
+//     System.Console.WriteLine(string.Join(", ", array));
+// }
+
+// Задача 40. Напишите программу, которая принимает на вход три числа и проверяет, может ли существоваться треугольник
+// со сторонами такой длины.
+// *Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+
+
+
+
+// Задача 42. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
