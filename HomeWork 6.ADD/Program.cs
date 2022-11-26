db9f42b3ca5c746317c4ba78613e2b8886edf32d
Alexander <asukhushin@icloud.com>
2022-11-26 09:21:49 +0300

    ДЗ №6 - Дополнительная задача

--- HomeWork 6.ADD/Program.cs
+++ HomeWork 6.ADD/Program.cs
@@ -1,1 +1,26 @@
 ﻿// Дополнительно. Дан массив. Найдите MEX массива. MEX (Minimum EXcluded) - минимальный отсутствующий элемент.
+
+Console.Clear();
+
+int[] numbers = new[] {0, 2, 1, 7, 5, 3, 6};
+int mex = GetMEX(numbers);
+Console.WriteLine($"Минимальное целое неотрицательное число, которого нет в массиве: {mex}");
+Console.ReadKey();
+
+int GetMEX(int[] numbers)
+{
+    for (int mex = 0; mex < numbers.Length; mex++)
+    {
+        bool valueMissing = true;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] == mex)
+            {
+                valueMissing = false;
+                break;
+            }
+        }
+        if (valueMissing){return mex;}
+    }
+    return numbers.Length;
+}
