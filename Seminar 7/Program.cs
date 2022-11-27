066a22b0a0903698413ec6af5f00ff1ecae0a962
Alexander <asukhushin@icloud.com>
2022-11-27 12:30:42 +0300

    Семинар 7

--- Seminar 7/Program.cs
+++ Seminar 7/Program.cs
@@ -1,35 +1,39 @@
 ﻿// Семинар 7
 
-// Задача 46. Задайте двумерный массив размером mхn, заполненный случайными числами
+// Задача 46. Задайте двумерный массив размером, заполненный случайными числами
 
 Console.Clear();
 
-int[,] array = GetArrayRandom(3, 4);
-PrintArray(array);
+// int[,] array = GetArrayRandom(3, 4);
+// PrintArray(array);
 
-int[,] GetArrayRandom(int rows, int cols, int minValue = 0, int maxValue = 100)
-{
-    int[,] array = new int[rows, cols];
+// int[,] GetArrayRandom(int rows, int cols, int minValue = 0, int maxValue = 1000)
+// {
+//     int[,] array = new int[rows, cols];
 
-    var rnd = new Random();
+//     var rnd = new Random();
 
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = rnd.Next(minValue, maxValue + 1);
-        }
-    }
-    return array;
-}
-void PrintArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-}
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = rnd.Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return array;
+// }
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// Задача 48. Задайте двумерный массив mxn, каждый элемент которого находится по формуле: A=m+n.
+// Выведите полученный массив на экран.
