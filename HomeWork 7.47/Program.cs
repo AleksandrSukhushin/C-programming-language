55eaecd6c93c215249d4158cc0511491443969aa
Alexander <asukhushin@icloud.com>
2022-11-27 14:04:06 +0300

    ДЗ№7 - Задача 47

--- HomeWork 7.47/Program.cs
+++ HomeWork 7.47/Program.cs
@@ -4,3 +4,34 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+
+Console.Clear();
+
+double[,] array = GetArrayRandom(3, 4);
+PrintArray(array);
+
+double[,] GetArrayRandom(int rows, int cols)
+{
+    double[,] array = new double[rows, cols];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
