9590bd03b0cc333d8e16e8e324369959aa9b2498
Alexander <asukhushin@icloud.com>
2022-11-27 15:49:37 +0300

    ДЗ№7 - Задача 52

--- HomeWork 7.52/Program.cs
+++ HomeWork 7.52/Program.cs
@@ -4,3 +4,50 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+
+Console.Write("Количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Количество столбцов: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+
+int[,] massive = new int[rows, cols];
+FillArray(massive);
+
+Console.Write("Среднее арифметическое столбцов: ");
+for (int j = 0; j < massive.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        avarage = (avarage + massive[i, j]);
+    }
+    avarage = avarage / rows;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+PrintArray(massive);
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine("");
+    }
+}
