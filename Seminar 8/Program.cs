9ccfbfe550c10f5d29d98dc6c9bfa124eabb29df
Alexander <asukhushin@icloud.com>
2022-11-28 15:12:54 +0300

    Семинар 8: Задачи 53, 55, 57, 59, 61

--- Seminar 8/Program.cs
+++ Seminar 8/Program.cs
@@ -10,24 +10,40 @@
 // 5 9 2 3
 // 1 4 7 2
 
+Console.Clear();
 
+int[,] ChangeArray(int[,] array)
+{
+    int temp;
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+    for (int i = 0; i < cols; i++)
+    {
+        temp = array[0, i];
+        array[0, i] = array[rows - 1, i];
+        array[rows - 1, i] = temp;
+    }
+    return array;
+}
 
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
 
+int[,] array = new int[,] {{1, 4, 7, 2},
+                           {5, 9, 2, 3},
+                           {8, 4, 2, 4}};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ChangeArray(array);
+PrintArray(array);
 
 
 // Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
