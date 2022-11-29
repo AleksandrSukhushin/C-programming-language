05cc35be6e42a96d37cb607e9d70cc44a5e94f08
Alexander <asukhushin@icloud.com>
2022-11-29 19:14:44 +0300

    ДЗ№8 - Задача 62

--- HomeWork 8.62/Program.cs
+++ HomeWork 8.62/Program.cs
@@ -4,3 +4,49 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+Console.Clear();
+
+int saze = 4;
+int[,] array = new int[saze, saze];
+
+void FillSpiral(int[,] array, int n)
+{
+    int i = 0;
+    int j = 0;
+    int value = 1;
+    for (int m = 0; m < n * n; m++)
+    {
+        int k = 0;
+        do
+        {
+            array[i, j++] = value++;
+        }
+        while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) array[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) array[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) array[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < 10)
+            {
+                Console.Write("0" + array[i, j]);
+                Console.Write(" ");
+            }
+            else Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillSpiral(array, saze);
+PrintArray(array);
