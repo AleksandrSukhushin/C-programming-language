aa286305751aaf6bb72be718c33a48f61b3c20e3
Alexander <asukhushin@icloud.com>
2022-11-29 18:41:23 +0300

    ДЗ№8 - Задача 60

--- HomeWork 8.60/Program.cs
+++ HomeWork 8.60/Program.cs
@@ -12,7 +12,7 @@ int[,,] array = new int[2, 2, 2];
 FillArray(array);
 PrintIndex(array);
 
-void PrintIndex(int[,,] arr)
+void PrintIndex(int[,,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -27,16 +27,16 @@ void PrintIndex(int[,,] arr)
     }
 }
 
-void FillArray(int[,,] arr)
+void FillArray(int[,,] array)
 {
     int number = 1;
-    for (int i = 0; i < arr.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            for (int k = 0; k < arr.GetLength(2); k++)
+            for (int k = 0; k < array.GetLength(2); k++)
             {
-                arr[k, i, j] += number;
+                array[k, i, j] += number;
                 number++;
             }
         }
