f72e87d1c7205d685b1a081fa594ddd1d27bb89a
Alexander <asukhushin@icloud.com>
2022-12-01 18:29:37 +0300

    ДЗ№9 - Задача 68

--- HomeWork 9.68/Program.cs
+++ HomeWork 9.68/Program.cs
@@ -1,3 +1,28 @@
 ﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+
+Console.Write("Введите число m больше 0: ");
+int m = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите число n больше 0: ");
+int n = Convert.ToInt32(Console.ReadLine()!);
+
+Console.WriteLine($"Значение функции Аккермана А(m,n): {Function(m,n)}");
+
+int Function(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Function(m - 1, 1);
+    }
+    else
+    {
+        return (Function(m - 1, Function(m, n - 1)));
+    }
+}
